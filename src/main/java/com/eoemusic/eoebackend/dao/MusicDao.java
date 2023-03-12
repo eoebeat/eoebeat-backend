1f223691ed614242e0c777df15906dc42d04c904
maxwellxiaoyuwu <xiaoyu.wu@northeastern.edu>
2023-03-11 21:25:53 -0800

    name alias

--- src/main/java/com/eoemusic/eoebackend/dao/MusicDao.java
+++ src/main/java/com/eoemusic/eoebackend/dao/MusicDao.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.util.UriUtils;
 
 /**
  * @description: some desc
@@ -106,8 +105,6 @@ public class MusicDao {
       res.setAudioUrl(new StringBuilder(alistUrlPrefix).append(data.get("Alist_audio_path"))
           .append(data.get("partial_url")).append(".").append(data.get("audio_media_type"))
           .toString());
-      String decodedUrl = UriUtils.decode(res.getAudioUrl(), "UTF-8");
-      System.out.println(decodedUrl);
       res.setCoverUrl(new StringBuilder(alistUrlPrefix).append(data.get("Alist_cover_path"))
           .append(data.get("partial_url")).append(".").append(data.get("cover_media_type"))
           .toString());
